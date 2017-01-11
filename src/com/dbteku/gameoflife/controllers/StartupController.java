fc88cf71033875116b3b67e3d9b9dd0a79eb6112
dbteku <dbteku@hotmail.com>
2017-01-10 19:13:58 -0700

    Init to project

---
+++ src/com/dbteku/gameoflife/controllers/StartupController.java
@@ -0,0 +1,17 @@
+package com.dbteku.gameoflife.controllers;
+
+import com.dbteku.orbit.core.OrbitEngine;
+
+public class StartupController {
+
+	private OrbitEngine engine;
+	
+	public StartupController() {
+		engine = new OrbitEngine();
+	}
+	
+	public void start(){
+
+	}
+	
+}
---
+++ src/com/dbteku/gameoflife/main/GameOfLife.java
@@ -0,0 +1,10 @@
+package com.dbteku.gameoflife.main;
+
+import com.dbteku.gameoflife.controllers.StartupController;
+
+public class GameOfLife {
+	public static void main(String[] args) {
+		StartupController controller = new StartupController();
+		controller.start();
+	}
+}
