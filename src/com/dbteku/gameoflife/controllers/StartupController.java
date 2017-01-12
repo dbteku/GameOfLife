9bd1b43725adf8d56a206e4242385371dff89ef1
= <dbteku@hotmail.com>
2017-01-12 11:55:29 -0700

    Map Updater

--- src/com/dbteku/gameoflife/controllers/StartupController.java
+++ src/com/dbteku/gameoflife/controllers/StartupController.java
@@ -1,6 +1,7 @@
 package com.dbteku.gameoflife.controllers;
 
 import com.dbteku.gamoeoflife.models.GameOfLifeWorld;
+import com.dbteku.gamoeoflife.models.MapUpdater;
 import com.dbteku.orbit.core.OrbitEngine;
 import com.dbteku.orbit.core.World;
 
@@ -9,14 +10,17 @@ public class StartupController {
 	private OrbitEngine engine;
 	private World world;
 	private MapController controller;
+	private MapUpdater updater;
 	
 	public StartupController() {
 		engine = new OrbitEngine();
 		world = new GameOfLifeWorld();
 		controller = new MapController(world, 10);
+		updater = new MapUpdater(controller);
 	}
 	
 	public void start(){
+		world.addObject(updater, 0, 0);
 		controller.createTiles();
 		engine.loadWorld(world);
 		engine.start();
---
+++ src/com/dbteku/gamoeoflife/models/MapUpdater.java
@@ -0,0 +1,71 @@
+package com.dbteku.gamoeoflife.models;
+
+import com.dbteku.gameoflife.controllers.MapController;
+import com.dbteku.orbit.animation.ImageAnimation;
+import com.dbteku.orbit.core.Actor;
+import com.dbteku.orbit.core.GameObject2D;
+import com.dbteku.orbit.core.World;
+import com.dbteku.orbit.models.ScreenEdge;
+
+import javafx.scene.input.KeyCode;
+
+public class MapUpdater extends Actor<MapUpdater> {
+
+	private final int FRAME_COUNT = 30;
+	private MapController controller;
+	private int currentFrame;
+	
+	public MapUpdater(MapController controller) {
+		this.controller = controller;
+		this.currentFrame = 0;
+	}
+	
+	@Override
+	public void act() {
+		currentFrame++;
+		if(currentFrame >= FRAME_COUNT){
+			checkTiles();
+			currentFrame = 0;
+		}
+	}
+	
+	private void checkTiles(){
+		System.out.println("RUNNING");
+	}
+
+	@Override
+	public void onAnimationUpdate() {
+
+	}
+
+	@Override
+	public void onTouchingEdge(World world, ScreenEdge edge) {
+
+	}
+
+	@Override
+	public void onCollision(GameObject2D object, World world, Class<?> cls) {
+
+	}
+
+	@Override
+	public void onKeyPressed(KeyCode code) {
+	
+	}
+
+	@Override
+	public void onKeyRelease(KeyCode code) {
+
+	}
+
+	@Override
+	public void onAnimationFinish(ImageAnimation<MapUpdater> animation) {
+
+	}
+
+	@Override
+	public void onClick() {
+
+	}
+
+}
